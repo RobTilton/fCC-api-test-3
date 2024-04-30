8367dcc9ab9d600347eee8b6a7b5f2a811f54898
Robert Tilton <RobertJTilton@gmail.com>
2024-04-30 19:56:59 +0000

    init commits

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+
+const mongoose = require('mongoose');
+const dns = require('dns');
+const urlparser = require('url');
 // Basic Configuration
+
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -14,10 +21,47 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+mongoose.connect(process.env.MONGO_DB)
+  .then(() => console.log('MongoDB Connected Successfully'))
+  .catch(err => console.log('MongoDB Connection Error', err));
+
+const urlSchema = new mongoose.Schema({
+  original_url: String,
+  short_url: Number
 });
+
+const Url = mongoose.model('Url', urlSchema);
+
+
+// Your first API endpoint
+app.post('/api/shorturl', function(req, res) {
+  console.log(req.body)
+  const url = req.body.url
+  const hostname = urlparser.parse(url).hostname;
+  
+  if(!hostname) {
+    return res.json({ error: "invalid url"});
+  }
+  
+  dns.lookup(hostname, async ( err, address ) =>{
+    if (err || !address) {
+      res.json({ error: "invalid url"});
+    } else {
+      const urlCount = await Url.countDocuments();
+      const newUrl = new Url({
+        original_url: url,
+        short_url: urlCount +1
+      });
+
+      try {
+        const result= await newUrl.save();
+        res.json({ original_url: url, short_url: result.short_url});
+      } catch (error) {
+        res.status(500).json({ error: "Server error"});
+      }
+    }
+  });
+}); 
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
